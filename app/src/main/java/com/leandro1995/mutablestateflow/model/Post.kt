0f21b05212b3c78fadf10d2b3c66328aec0d9063
Leandro Castillo Borja <lcastillo@peruapps.com.pe>
2021-06-22 01:04:40 -0500

    Crear modelo post

---
+++ app/src/main/java/com/leandro1995/mutablestateflow/model/Post.kt
@@ -0,0 +1,8 @@
+package com.leandro1995.mutablestateflow.model
+
+class Post constructor(
+    var userId: Int = -1,
+    var id: Int = -1,
+    var title: String = "",
+    var body: String = ""
+)
