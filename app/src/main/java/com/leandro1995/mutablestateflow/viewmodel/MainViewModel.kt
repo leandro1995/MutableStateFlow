5e10c70ce1960b51e26266099f3b29907566753e
Leandro Castillo Borja <lcastillo@peruapps.com.pe>
2021-06-22 11:05:08 -0500

    Implementar eventos en main activity

--- app/src/main/java/com/leandro1995/mutablestateflow/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/leandro1995/mutablestateflow/viewmodel/MainViewModel.kt
@@ -4,4 +4,15 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    companion object {
+        const val LOAD_LIST = 1
+    }
+
+    val onclick = fun(action: Int) {
+        when (action) {
+            LOAD_LIST -> {
+
+            }
+        }
+    }
 }
