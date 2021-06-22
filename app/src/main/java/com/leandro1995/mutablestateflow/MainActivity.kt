3ee711a638f991176e40333869e9b10943f512fb
Leandro Castillo Borja <lcastillo@peruapps.com.pe>
2021-06-22 10:54:24 -0500

    Colocar viewModel en mainActivity

--- app/src/main/java/com/leandro1995/mutablestateflow/MainActivity.kt
+++ app/src/main/java/com/leandro1995/mutablestateflow/MainActivity.kt
@@ -2,10 +2,22 @@ package com.leandro1995.mutablestateflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.leandro1995.mutablestateflow.databinding.ActivityMainBinding
+import com.leandro1995.mutablestateflow.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        activityMainBinding.mainViewModel = mainViewModel
     }
 }
---
+++ app/src/main/java/com/leandro1995/mutablestateflow/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.leandro1995.mutablestateflow.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
