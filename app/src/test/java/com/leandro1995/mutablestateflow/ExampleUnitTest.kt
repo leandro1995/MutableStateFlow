5540d3cd81a679ca2e0984ad01c77d8b42fbd1c7
Leandro Castillo Borja <lcastillo@peruapps.com.pe>
2021-06-23 18:34:35 -0500

    Implementar test de lista

--- app/src/test/java/com/leandro1995/mutablestateflow/ExampleUnitTest.kt
+++ app/src/test/java/com/leandro1995/mutablestateflow/ExampleUnitTest.kt
@@ -1,17 +1,57 @@
 package com.leandro1995.mutablestateflow
 
+import com.leandro1995.mutablestateflow.config.Setting
+import com.leandro1995.mutablestateflow.model.design.PostListLoading
+import com.leandro1995.mutablestateflow.model.entity.Post
+import com.leandro1995.mutablestateflow.viewmodel.MainViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@RunWith(JUnit4::class)
 class ExampleUnitTest {
+
+    private val mainViewModel = MainViewModel()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun listEmpty() {
+        mainViewModel.apply {
+
+            postLoadingFlow.value =
+                PostListLoading(code = Setting.MESSAGE_WARNING, errorMessage = Setting.LIST_EMPTY)
+
+            showPostList(postList = postLoadingFlow.value.postList)
+
+            assertEquals(Setting.LIST_EMPTY, postLoadingFlow.value.errorMessage)
+        }
+    }
+
+    @Test
+    fun list() {
+        mainViewModel.apply {
+
+            postLoadingFlow.value = PostListLoading(
+                postList = mutableListOf(
+                    Post(
+                        userId = 1,
+                        id = 2,
+                        title = "test",
+                        body = "test"
+                    )
+                )
+            )
+
+            showPostList(postList = postLoadingFlow.value.postList)
+
+            assertNotEquals(0, postLoadingFlow.value.postList.size)
+        }
     }
 }
